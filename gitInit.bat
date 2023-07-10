414103865aeaa65e12b51d402ad114af04f59162
John Campbell <campbj49@gmail.com>
2023-07-10 15:57:09 -0400

    first commit to GitHub

---
+++ gitInit.bat
@@ -0,0 +1,6 @@
+git init
+git add --all
+git commit -m "first commit to GitHub"
+git branch -M main
+git remote add origin https://github.com/campbj49/Project9-FindIndexFind.git
+git push -u origin main
